99aa08664e388172d9947e7386beb431d0820bd8
Vinay Yadav <87544848+Vinayadav2016@users.noreply.github.com>
2021-08-09 19:02:40 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
-
+class Shape {
+  constructor(){
+     color = "red";
+  }
+  drawShape(){
+    console.log("draw Area");
+  }
+  calculateArea(){
+    console.log("calculate area");
+  }
+}
+module.exports = Shape;
 // export class using module.exports
